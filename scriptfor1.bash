96d9c67d0e1de0d07a34531ed4e9987f58f356a0
root <greg6fr@gmail.com>
2026-06-12 08:05:12 +0000

    script for in list

---
+++ scriptfor1.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Author: Jules GREGOIRE
+#Description: for in script
+#DateCreated: 12/06/2026
+#DateModified: 12/06/2026
+
+for i in 1 2 3 4 5
+do
+echo "bonjour mon fichier$i" >> monfichier$i.txt
+done
